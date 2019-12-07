1a526b6e0f83f2b176ecad4d83300deb30a34898
Vahid Jafari <32508725+vajafari@users.noreply.github.com>
2019-12-07 08:58:08 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _http = _interopRequireDefault(require("http"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// eslint-disable-next-line no-console
+console.log(_http["default"].get('http://googl.com'));
